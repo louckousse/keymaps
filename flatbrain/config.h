a446c9584a2f6043d4de409479fe93d3748cfcf6
louckousse <louckousse@flatbrains.me>
2021-05-10 10:19:04 +0200

    v2

--- flatbrain/config.h
+++ flatbrain/config.h
@@ -12,24 +12,20 @@
 #define DESCRIPTION     Unsplitted 40 percent column staggered keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 8
-#define MATRIX_COLS 6
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { F5, F4, B4, B5, D4, C6, D7, E6 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
-#define UNUSED_PINS
+#define MATRIX_ROW_PINS { B0, D2, F4, F5 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6, B5, B4, E6, D7, C6, D4 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
+#define ENCODERS_PAD_A { F1, B7 }
+#define ENCODERS_PAD_B { F0, D5 }
+
 /* number of backlight levels */
-#ifdef RGBLIGHT_ENABLE
-  #define RGBLIGHT_ANIMATIONS
-  #define RGBLIGHT_HUE_STEP 8
-  #define RGBLIGHT_SAT_STEP 8
-  #define RGBLIGHT_VAL_STEP 8
-#endif
 
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X64
@@ -40,11 +36,12 @@
 
 #define RGB_DI_PIN D3
 #ifdef RGB_DI_PIN
-#define RGBLED_NUM 10
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#define RGBLIGHT_LAYERS
+    #define RGBLED_NUM 10
+    #define RGBLIGHT_HUE_STEP 5
+    #define RGBLIGHT_SAT_STEP 5
+    #define RGBLIGHT_VAL_STEP 5
+    #define RGBLIGHT_LAYERS
+    #define RGBLIGHT_LIMIT_VAL 150
 #endif
 
 #endif
